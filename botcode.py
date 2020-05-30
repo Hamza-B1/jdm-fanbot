314fd58a1ebd2d48e42584a8bf64bead873d118d
IntegrX <66087630+IntegrX@users.noreply.github.com>
2020-05-30 22:53:02 +0100

    Add files via upload

---
+++ botcode.py
@@ -0,0 +1,114 @@
+import discord
+from discord.ext import commands
+import discord.utils
+import json
+
+client = discord.Client()
+
+client = commands.Bot(command_prefix='£')
+
+jdm_id = 292626856509964288
+
+
+@client.command()
+async def update(ctx, *, input):
+    with open('project.json', 'r+') as f:
+        messagetest = json.load(f)
+        messagetest.append({"Message": input})
+        json.dump(messagetest, f)
+        await ctx.send('Log updated')
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    await client.change_presence(activity=discord.Game(name='Borgar'))
+
+
+@client.event
+async def on_message_delete(ctx):
+    for channel in ctx.guild.text_channels:
+        if channel.name == 'logs':
+            embed = discord.Embed(title=f'Message Deleted in #{ctx.channel}',
+                                  colour=discord.Colour.dark_red()
+                                  )
+            embed.add_field(name='Message Author', value=f'{ctx.author} | {ctx.author.id}', inline=False)
+            embed.add_field(name='Content:', value=f'{ctx.content}', inline=True)
+            embed.add_field(name=f'Message ID: ', value=f'{ctx.id}', inline=False)
+            embed.set_thumbnail(url=ctx.author.avatar_url)
+            await channel.send(embed=embed)
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+
+@client.command()
+async def diophantine(ctx, *, input):
+    eq = ''.join(input.split())
+    a = int(eq.split("x")[0])
+    b_route = eq.split("+")
+    b = int((b_route[1].split("y")[0]))
+    c = int(eq.split("=")[1])
+    embed = discord.Embed(
+        title='Equation Decomposition',
+
+        description=f'x coefficient = {a}\n y coefficient = {b}\n constant = {c}',
+        colour=discord.Colour.dark_red()
+    )
+    await ctx.send(embed=embed)
+
+
+# clear block
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount=10):
+    await ctx.channel.purge(limit=amount + 1)
+    await ctx.send(f'{amount} messages purged.')
+
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    if member == ctx.author:
+        embed0 = discord.Embed(description='You cannot kick yourself.', colour=discord.Colour.dark_red())
+        await ctx.send(embed=embed0)
+        return
+    else:
+        await member.kick(reason=reason)
+        embed = discord.Embed(title=f'{member} Kicked:', description=f'Reason : {reason}\n\n How '
+                                                                     f'bout next time you join when you can '
+                                                                     f'handle the Abdul Style :sunglasses::metal:'
+                              , colour=discord.Colour.dark_red())
+        await ctx.send(embed=embed)
+        for channel in ctx.guild.text_channels:
+            if channel.name == 'logs':
+                embed2 = discord.Embed(title=f'{member} Kicked by {ctx.author}', description=f'Reason : {reason}')
+                embed2.set_thumbnail(url=member.avatar_url)
+                await channel.send(embed=embed2)
+
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def silence(ctx, member: discord.Member, *, reason=None):
+    for role in ctx.guild.roles:
+        if role.name == 'Muted':
+            await member.add_roles(role)
+    for role1 in ctx.guild.roles:
+        if role1.name == 'Zumalaa':
+            await member.remove_roles(role1)
+    await ctx.send(f'{member} was muted. Reason: {reason}')
+
+@client.command()
+@commands.has_permissions(manage_messages=True)
+async def unsilence(ctx, member: discord.Member):
+    for role in ctx.guild.roles:
+        if role.name == 'Muted':
+            await member.remove_roles(role)
+    for role1 in ctx.guild.roles:
+        if role1.name == 'Zumalaa':
+            await member.add_roles(role1)
+    await ctx.send(f'{member} was unmuted.')
+
+client.run("NDMzNjY4MzEzNTYzMDA0OTI4.XriBWg.7fb9u9IMEJocfIUFVdCCv5jlzg0")
