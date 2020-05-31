f54a01cdf7083c41617df610dbc085e61ac5b83c
IntegrX <66087630+IntegrX@users.noreply.github.com>
2020-05-31 23:18:59 +0100

    Add files via upload

--- botcode.py
+++ botcode.py
@@ -18,12 +18,11 @@ async def on_ready():
 
 
 @client.event
-async def on_member_join(ctx, member):
-    if ctx.guild.id == 679781666423570480:
-        channel = client.get_channel(715991568179265536)
-        await channel.send(f'Welcome {member}! To gain access to the rest of the server, '
-                           f'you need to talk here for a little while '
-                           f'to level up. Enjoy your stay :sunglasses::metal:')
+async def on_member_join(member):
+    channel = client.get_channel(715991568179265536)
+    await channel.send(f'Welcome {member}! To gain access to the rest of the server, '
+                       f'you need to talk here for a little while '
+                       f'to level up. Enjoy your stay :sunglasses::metal:')
 
 
 @client.event
