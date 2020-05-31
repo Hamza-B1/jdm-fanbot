ca2e4feebdedf78be1cf2ae22fe1a06fb76cb31e
IntegrX <66087630+IntegrX@users.noreply.github.com>
2020-05-31 18:27:53 +0100

    Add files via upload

--- botcode.py
+++ botcode.py
@@ -116,12 +116,11 @@ async def halaqa(ctx):
     for role in ctx.author.roles:
         if role.name == 'Halaqa':
             await ctx.send('You already have this role!')
-
-    else:
-        for role1 in ctx.guild.roles:
-            if role1.name == 'Halaqa':
-                await ctx.author.add_roles(role1)
-                await ctx.send('You now have the Halaqa role.')
+            return
+    for role1 in ctx.guild.roles:
+        if role1.name == 'Halaqa':
+            await ctx.author.add_roles(role1)
+            await ctx.send('You now have the Halaqa role.')
 
 
 
