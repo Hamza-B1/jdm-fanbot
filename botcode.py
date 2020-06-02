d301a23b37537ab6a0397f3923d5a9185ce09c63
IntegrX <66087630+IntegrX@users.noreply.github.com>
2020-06-02 18:35:20 +0100

    Add files via upload

--- botcode.py
+++ botcode.py
@@ -10,6 +10,7 @@ client.remove_command('help')
 
 jdm_id = 292626856509964288
 
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -18,10 +19,11 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    channel = client.get_channel(715991568179265536)
-    await channel.send(f'As salaam alaykum {member.mention}! To gain access to the rest of the server, '
-                       f'you need to talk here for a little while '
-                       f'to level up. Enjoy your stay :sunglasses::metal:')
+    if member.guild.id == 679781666423570480:
+        channel = client.get_channel(715991568179265536)
+        await channel.send(f'As salaam alaykum {member.mention}! To gain access to the rest of the server, '
+                           f'you need to talk here for a little while '
+                           f'to level up. Enjoy your stay :sunglasses::metal:')
 
 
 @client.event
@@ -134,8 +136,6 @@ async def students(ctx):
         if role1.name == 'Students':
             await ctx.author.add_roles(role1)
             await ctx.send('You now have the Student role.')
-
-
 
 
 client.run("NDMzNjY4MzEzNTYzMDA0OTI4.XriBWg.7fb9u9IMEJocfIUFVdCCv5jlzg0")
