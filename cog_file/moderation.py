2a820750854dbb982195fa3548fc350045602d18
IntegrX <66087630+IntegrX@users.noreply.github.com>
2020-07-07 18:57:32 +0100

    Add files via upload

--- cog_file/moderation.py
+++ cog_file/moderation.py
@@ -145,10 +145,11 @@ class Moderation(commands.Cog):
                                 await ctx.send('Inquiry updated. Thank you!')
                                 break
                     elif 'no' in msg.content:
-                        await ctx.send('Inquiry ended')
                         break
                     else:
                         continue
+            await ctx.send('Inquiry ended')
+
 
 
 def setup(client):
