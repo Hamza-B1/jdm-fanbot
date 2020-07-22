c102d449bc636be9fa1cab76b4c8d69698802cf7
IntegrX <66087630+IntegrX@users.noreply.github.com>
2020-07-22 14:47:19 +0100

    Add files via upload

--- botcodev2.py
+++ botcodev2.py
@@ -20,7 +20,7 @@ cur = conn.cursor()
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Loading Available Cog Files and Printing Online Status in Log
-cog_list = ['moderation', 'basics']
+cog_list = ['moderation', 'basics', 'voice']
 
 for cog in cog_list:
     client.load_extension(f'cog_file.{cog}')
@@ -38,37 +38,6 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
-@client.command()
-async def join(ctx):
-    channel = ctx.message.author.voice.channel
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
-
-    await voice.disconnect()
-
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        await channel.connect()
-    await ctx.send(f"Joined {channel}")
-
-
-@client.command()
-async def leave(ctx):
-    channel = ctx.message.author.voice.channel
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-
-    if voice and voice.is_connected():
-        await voice.disconnect()
-        await ctx.send(f"Left {channel}")
-    else:
-        await ctx.send("Don't think I am in a voice channel")
-
 # ----------------------------------------------------------------------------------------------------------------------
 # Run Bot Using Token
-
 client.run(TOKEN)
