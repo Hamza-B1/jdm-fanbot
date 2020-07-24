16f6493d0579a7c0e27b13532226a079dc0ed5c2
IntegrX <66087630+IntegrX@users.noreply.github.com>
2020-07-24 19:42:48 +0100

    Add files via upload

--- cog_file/moderation.py
+++ cog_file/moderation.py
@@ -289,10 +289,10 @@ class Moderation(commands.Cog):
                 self.cur.execute("INSERT INTO chrono_tasks VALUES(DEFAULT, 'mute', %s, %s);", (time.time(), time.time() + total))
         self.cur.execute("SELECT * FROM mod_actions ORDER BY action_id DESC LIMIT 1;")
         value = self.cur.fetchone()
-        self.cur.execute("SELECT * FROM mod_actions ORDER BY ")
+        self.cur.execute("SELECT * FROM chrono_tasks ORDER BY action_id DESC LIMIT 1;")
         await member.add_roles(discord.utils.get(ctx.guild.roles, name='Muted'))
         await ctx.send(f"Test success: Member muted for {total}. Action ID: {value[0]}\nUnix epoch start time{time.time()}\nUnix epoch finish time {time.time() + total}")
-        
+
 
 
 def setup(client):
