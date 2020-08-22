7f0943654a355207137c95225ca63599dacd667f
IntegrX <66087630+IntegrX@users.noreply.github.com>
2020-08-22 23:38:00 +0100

    Add files via upload

--- cog_file/moderation.py
+++ cog_file/moderation.py
@@ -49,7 +49,7 @@ class Moderation(commands.Cog):
             return m.author == message.author and m.content == message.content
         new_message = self.client.wait_for('message', check=check, timeout=0.8)
         if new_message:
-            await self.client.delete_message(new_message)
+            await self.client.delete_messages([new_message, message])
         await self.client.process_commands()
 
     @commands.command()
